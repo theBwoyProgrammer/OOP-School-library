7bf3a51ce75364b1ea465d2fb93823ee5cc9c565
Tuyishimire Eric <tuyishimireericc@gmail.com>
2022-10-12 18:48:32 +0200

    Tested Rentals

--- tests.rb
+++ tests.rb
@@ -49,8 +49,13 @@ describe Classroom do
 end
 
 describe Rental do
-  it 'has a date' do
-    rental = Rental.new(date)
-    expect(rental.date).to eq(12 / 13 / 2020)
+  it 'Test Rentals' do
+    teacher = Teacher.new(1, 18, 'John', 'Math')
+    book = Book.new('Abdollah', 'Maramagambo')
+    rental = Rental.new('2022/10/12', book, teacher)
+
+    expect(rental.date).to eq '2022/10/12'
+    expect(rental.person.name).to eq 'John'
+    expect(rental.book.title).to eq 'Maramagambo'
   end
 end
